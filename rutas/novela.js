1bd9d8fae94370c72315d6c052b958a5687627bd
Yago Rayon <yago.rayon@sisnet360.com>
2023-06-02 13:48:17 +0200

    Update de novela hecho.

--- rutas/novela.js
+++ rutas/novela.js
@@ -21,7 +21,7 @@ const schemaCapitulo = Joi.object({
 })
 
 router.post('/nueva', validarToken, async (req, res) => {
-    if (req.usuario.rol && req.usuario.rol != 'Usuario' && req.usuario.rol != 'Admin'){
+    if (req.usuario.rol && req.usuario.rol != 'Usuario' && req.usuario.rol != 'Admin') {
         return res.status(401).json({ error: 'No tienes permisos para crear una novela.' })
     }
 
@@ -40,31 +40,31 @@ router.post('/nueva', validarToken, async (req, res) => {
             { error: 'El titulo no está disponible' }
         )
     }
-    
+
     try {
-        
-        if (req.usuario.email){
-            let usuario = await Usuario.findOne({email : req.usuario.email});
-            if (!usuario){
+
+        if (req.usuario.email) {
+            let usuario = await Usuario.findOne({ email: req.usuario.email });
+            if (!usuario) {
                 return res.status(400).json(
                     { error: 'Error al buscar el usuario en la base de datos' }
                 )
             }
-            let autorNovela = { autor_id : usuario._id, autorNickname : usuario.nickname }
+            let autorNovela = { autor_id: usuario._id, autorNickname: usuario.nickname }
             let nombreImagen = 'placeholder.png';
-            if (req.file){
+            if (req.file) {
                 let imagen = req.file;
                 const extensionImagen = mime.extension(imagen.mimetype);
                 nombreImagen = imagen.originalname.split(extensionImagen)[0] + '-' + Date.now() + '.' + extensionImagen;
-                let rutaImagen = 'assets/img/'+nombreImagen;
-                fs.writeFile( rutaImagen , req.file.buffer, (error)=>{
-                    if (error){
+                let rutaImagen = 'assets/img/' + nombreImagen;
+                fs.writeFile(rutaImagen, req.file.buffer, (error) => {
+                    if (error) {
                         return res.status(400).json({ error: 'Error al subir imagen' })
                     }
                 });
             }
             const novela = new Novela({
-                autor : autorNovela,
+                autor: autorNovela,
                 titulo: req.body.titulo,
                 descripcion: req.body.descripcion,
                 generos: req.body.generos,
@@ -72,14 +72,14 @@ router.post('/nueva', validarToken, async (req, res) => {
                 imagen: nombreImagen
             });
             const novelaGuardada = await novela.save();
-            if(novelaGuardada){
-                const novelaPublicada = { novela_id: novelaGuardada._id, titulo : novelaGuardada.titulo, descripcion : novelaGuardada.descripcion, fechaCreacion: novelaGuardada.fechaCreacion, imagen: novelaGuardada.imagen };
+            if (novelaGuardada) {
+                const novelaPublicada = { novela_id: novelaGuardada._id, titulo: novelaGuardada.titulo, descripcion: novelaGuardada.descripcion, fechaCreacion: novelaGuardada.fechaCreacion, imagen: novelaGuardada.imagen };
                 usuario.novelasPublicadas.push(novelaPublicada);
                 await usuario.save();
             }
-        }else{
+        } else {
             res.status(400).json({ error: 'Error al crear novela' })
-        } 
+        }
     } catch (error) {
         res.status(400).json({ error })
         return;
@@ -91,34 +91,38 @@ router.post('/nueva', validarToken, async (req, res) => {
 })
 
 router.delete('/:_id', validarToken, async (req, res) => {
-    if (req.usuario.rol && req.usuario.rol != 'Usuario' && req.usuario.rol != 'Admin'){
+    if (req.usuario.rol && req.usuario.rol != 'Usuario' && req.usuario.rol != 'Admin') {
         return res.status(401).json({ error: 'No tienes permisos para eliminar una novela.' })
     }
+    if (!req.params._id) {
+        return res.status(401).json({ error: 'Error al recibir parámetros' })
+    }
     try {
-        console.log(req.params._id.trim());
-        if (req.params._id){
-            let novela = await Novela.findById( req.params._id );
-            let usuario = await Usuario.findById( novela.autor.autor_id );
-            if (!usuario){
-                return res.status(400).json(
-                    { error: 'Error al buscar el usuario en la base de datos' }
-                )
-            }
-            if( req.usuario.email != usuario.email ){
-                return res.status(400).json(
-                    { error: 'Usuario incorrecto' }
-                )
-            }
-            if(novela){
-                let posicionNovela = usuario.novelasPublicadas.findIndex((elemento) => {console.log(elemento.novela_id); return elemento.novela_id.toString() == novela._id });
-                usuario.novelasPublicadas.splice(posicionNovela,1);
-                fs.rm('assets/img/'+novela.imagen);
-                await Novela.findByIdAndDelete(novela._id);
-                await usuario.save();
-            }
-        }else{
-            res.status(400).json({ error: 'Error al crear novela' })
-        } 
+        let novela = await Novela.findById(req.params._id);
+        let usuario = await Usuario.findById(novela.autor.autor_id);
+        if (!usuario) {
+            return res.status(400).json(
+                { error: 'Usuario incorrecto' }
+            )
+        }
+        if (!novela) {
+            return res.status(400).json(
+                { error: 'Error al recuperar la novela' }
+            )
+        }
+        if (req.usuario.email != usuario.email) {
+            return res.status(400).json(
+                { error: 'Usuario incorrecto' }
+            )
+        }
+        let posicionNovela = usuario.novelasPublicadas.findIndex((elemento) => elemento.novela_id.toString() == novela._id);
+        usuario.novelasPublicadas.splice(posicionNovela, 1);
+        let rutaImagen = 'assets/img/' + novela.imagen;
+        if (novela.imagen != 'placeholder.png' && fs.existsSync(rutaImagen)) {
+            fs.rm(rutaImagen);
+        }
+        await Novela.findByIdAndDelete(novela._id);
+        await usuario.save();
     } catch (error) {
         res.status(400).json({ error })
         return;
@@ -130,23 +134,22 @@ router.delete('/:_id', validarToken, async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-    
     let opciones = {};
     opciones.page = req.query.pagina || 1;
     opciones.limit = req.query.limite || 25;
-    if(req.query.ordenar){
+    if (req.query.ordenar) {
         const direccion = req.query.direccion || "desc";
-        opciones.sort={};
-        opciones.sort[req.query.ordenar]= direccion;
+        opciones.sort = {};
+        opciones.sort[req.query.ordenar] = direccion;
     }
     let busqueda = {};
-    if(req.query.genero){
-        busqueda.generos = req.query.genero.split(',');
+    if (req.query.genero) {
+        busqueda.generos = req.query.genero;
     }
-    if(req.query.etiqueta){
+    if (req.query.etiqueta) {
         busqueda.etiquetas = req.query.etiqueta;
     }
-    const novelas = await Novela.paginate(busqueda,opciones)
+    const novelas = await Novela.paginate(busqueda, opciones)
 
     if (!novelas) {
         return res.status(400).json(
@@ -156,6 +159,83 @@ router.get('/', async (req, res) => {
     return res.status(200).json(
         { novelas: novelas }
     )
+})
+
+router.put('/:_id', validarToken, async (req, res) => {
+    if (req.usuario.rol && req.usuario.rol != 'Usuario' && req.usuario.rol != 'Admin') {
+        return res.status(401).json({ error: 'No tienes permisos para editar una novela.' })
+    }
+    if (!req.params._id) {
+        return res.status(401).json({ error: 'Error al recibir parámetros' })
+    }
+    // Validar Novela
+    const { error } = schemaNovela.validate(req.body)
+    if (error) {
+        return res.status(400).json(
+            { error: error.details[0].message }
+        )
+    }
+
+    // Validar titulo único 
+    let novela = await Novela.findById(req.params._id);
+    if (!novela) {
+        return res.status(400).json(
+            { error: 'La novela a editar no existe' }
+        )
+    }
+
+    try {
+        if (req.usuario.email) {
+            let usuario = await Usuario.findById(novela.autor.autor_id);
+            if (!usuario || usuario.email != req.usuario.email) {
+                return res.status(400).json(
+                    { error: 'Error de usuario' }
+                )
+            }
+            let nombreImagen = novela.imagen;
+            if (req.file) {
+                let  rutaImagenABorrar = 'assets/img/'+novela.imagen;
+                console.log(rutaImagenABorrar);
+                if (novela.imagen != 'placeholder.png' && fs.existsSync(rutaImagenABorrar)){
+                    fs.rm(rutaImagenABorrar,(error)=>{ if (error) {
+                        return res.status(400).json({ error: 'Error al subir imagen' })
+                    } });
+                }
+                let imagen = req.file;
+                const extensionImagen = mime.extension(imagen.mimetype);
+                nombreImagen = imagen.originalname.split('.')[0] + '-' + Date.now() + '.' + extensionImagen;
+                let rutaImagen = 'assets/img/' + nombreImagen;
+                fs.writeFile(rutaImagen, req.file.buffer, (error) => {
+                    if (error) {
+                        return res.status(400).json({ error: 'Error al subir imagen' })
+                    }
+                });
+            }
+            const novelaGuardada = await Novela.findByIdAndUpdate(novela._id,{ 
+                titulo : req.body.titulo || novela.titulo, 
+                descripcion: req.body.descripcion || novela.descripcion,
+                generos: req.body.generos || novela.generos,
+                etiquetas : req.body.etiquetas || novela.etiquetas,
+                imagen : nombreImagen
+            })
+            if (novelaGuardada) {
+                const novelaEditada = { novela_id: novelaGuardada._id, titulo: novelaGuardada.titulo, descripcion: novelaGuardada.descripcion, fechaCreacion: novelaGuardada.fechaCreacion, imagen: novelaGuardada.imagen };
+                let posicionNovela = usuario.novelasPublicadas.findIndex((elemento) => elemento.novela_id.toString() == novela._id);
+                Object.assign(usuario.novelasPublicadas[posicionNovela], novelaEditada);
+                // console.log(usuario.novelasPublicadas);
+                await usuario.save();
+            }
+        } else {
+            res.status(400).json({ error: 'Error al crear novela' })
+        }
+    } catch (error) {
+        res.status(400).json({ error: 'En algun lado hay un error' })
+        return;
+    }
+    return res.status(200).json({
+        error: null,
+        data: 'Novela creada con éxito'
+    })
 })
 
 module.exports = router;
